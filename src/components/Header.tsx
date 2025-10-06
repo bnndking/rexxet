04acc1192aaa55fbee18d9fdab24a321f5225b30
bnndking <bnd.developerz@gmail.com>
2025-10-06 18:09:49 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Menu, X } from 'lucide-react';
+import { useNavigate } from 'react-router-dom'; // 
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
